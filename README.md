2a7aaac7cef869b1a8e80e2aac818deed603c759
Jessica Campbell <jessicacampbell@zipcodersMBP42.home>
2018-05-09 22:11:53 -0400

    after 8 hrs

--- README.md
+++ README.md
@@ -12,3 +12,22 @@ This repo contains an exercise intended for Back-End Engineers.
 ## Expectations
 1. Please take no more than 8 hours to work on this exercise. Complete as much as possible and then submit your solution.
 1. This exercise is meant to showcase how you work. With consideration to the time limit, do your best to treat it like a production system.
+
+
+## Solution
+- So I decided to make a file reader that would parse the data into Java objects (POJOs)
+- This was done by splitting the csv file into an array by the commas that separated the values
+- Once that was done I decided to add any entry to an ArrayList of entries
+- I then had to also create a Watch service to see whenever a file was updated, created, or deleted
+
+##Future Steps
+- I was not able to complete the program in 8 hours because there were some things I was not familiar with so I decided to research these areas
+- Things I was not able to accomplish but would build on:
+    - Making sure that all the information was not empty and how to handle that with either default values or blank values
+    - Using Regex to confirm the phone numbers were in ###-###-#### format
+    -  Putting a maximum count on the Strings for all names and id number
+    
+##Areas of Improvement
+- Overall this was a great learning experience for me - If I had a little more time I would have been able to build out the program more effectively
+- However, I learned what a csv file was and how it will relate to a job setting / real work environment
+- Thank you for this opportunity
--- src/main/java/JsonParser.java
+++ src/main/java/JsonParser.java
@@ -1,11 +1,42 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.*;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class JsonParser {
 
-    public static void main(String[] args) {
+    private ArrayList<FullEntry> entries;
+
+    public JsonParser(){
+        entries = new ArrayList<>();
+    }
+
+    public void loadDataFromFile(String filepath){
+        try{
+            Scanner scanner = new Scanner(new FileReader(filepath));
+            String line;
+            FullEntry fullEntry;
+            scanner.nextLine();
+            while (scanner.hasNextLine());
+            line = scanner.nextLine();
+            String[] results = line.split(",");
+            String id = results[0];
+            String firstName = results[1];
+            String middleName = results[2];
+            String lastName = results[3];
+            String phoneNumber = results[4];
+
+            Name name = new Name(firstName, middleName, lastName);
+            fullEntry = new FullEntry(id, name, phoneNumber);
+            entries.add(fullEntry);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void watchService() {
         try(WatchService service = FileSystems.getDefault().newWatchService()){
             Map<WatchKey, Path> keyPathMap = new HashMap<>();
             Path path = Paths.get("/Users/jessicacampbell/Dev/be_exam_candidate_jc/src/main/resources/input-directory/input.csv");
@@ -18,7 +49,6 @@ public class JsonParser {
             WatchKey watchKey;
             do {
                 watchKey = service.take();
-                Path eventDir = keyPathMap.get(watchKey);
             }
                 while (watchKey.reset());
 
